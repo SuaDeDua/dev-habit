8dafeddaa9a338f2e16390f6ceab83746046f392
SuaDeDua <sokvai123hn@gmail.com>
2025-10-31 17:49:13 +0700

    add little database

--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,3 +1,6 @@
+using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -8,6 +11,14 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options
+         .UseNpgsql(
+             builder.Configuration.GetConnectionString("Database"),
+             npgsqlOptions => npgsqlOptions
+                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+         .UseSnakeCaseNamingConvention());
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
