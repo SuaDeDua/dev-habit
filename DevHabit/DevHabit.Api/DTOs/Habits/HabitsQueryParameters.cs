765d7c07ae8db4a948aaf034c605112257f18eea
SuaDeDua <sokvai123hn@gmail.com>
2025-11-20 17:05:09 +0700

    update: Supporting Data Shaping

--- DevHabit/DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
+++ DevHabit/DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
@@ -15,6 +15,8 @@ public sealed record HabitsQueryParameters
 
     public string? Sort { get; init; }
 
+    public string? Fields { get; init; }
+
     public int Page { get; init; } = 1;
 
     public int PageSize { get; init; } = 10;
